4a1fce2b7c6bc270b660650f4c8145ee0ca68c18
Dario Rodrigo Torres Messenzani <54510225+Rodrigo00909@users.noreply.github.com>
2021-06-25 17:04:20 -0300

    info styled + data

---
+++ src/data/InfoData.js
@@ -0,0 +1,22 @@
+import ImageOne from '../images/imagethree.jpg';
+import ImageTwo from '../images/imageone.jpg';
+
+export const infoData = {
+    heading: 'Look this planets',
+    paragraphOne: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+    paragraphTwo: '2 Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+    button: 'View Planets',
+    image: ImageOne,
+    reverse: false,
+    delay: 100
+};
+
+export const infoDataTwo = {
+    heading: 'Look this planets 2',
+    paragraphOne: 'Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+    paragraphTwo: '2 Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet',
+    button: 'View Planets 2',
+    image: ImageTwo,
+    reverse: false,
+    delay: 100
+};
